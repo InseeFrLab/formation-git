442772fad5cabab72cf60a9f49b7a2a55ae14614
Romain Lesur <romain.lesur@justice.gouv.fr>
2019-06-11 16:01:23 +0200

    use a more meaningful name for output directory

--- build.R
+++ build.R
@@ -2,6 +2,6 @@
 
 f = list.files('supports', 'Rmd$', full.names = TRUE, recursive = TRUE)
 o = sapply(f, function(f) rmarkdown::render(f, output_options = list(self_contained = TRUE)))
-dir.create('artifacts')
-copied = file.copy(o, 'artifacts')
+dir.create('html')
+copied = file.copy(o, 'html')
 stopifnot(all(copied))
