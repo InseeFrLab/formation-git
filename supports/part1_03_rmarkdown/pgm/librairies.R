6ddea9701bc27450ef1436727e0f2aa61e08e892
moineau <annie.moineau@insee.fr>
2019-11-07 14:12:45 +0000

    Intégration de la partie Rmd dans la branche principale

---
+++ supports/part1_03_rmarkdown/pgm/librairies.R
@@ -0,0 +1,15 @@
+# librairies
+
+# ------------------------------------------------------------------------------------------------
+# install.packages("questionr")
+# install.packages("datasets")
+# install.packages("gmodels")
+# install.packages("tidyverse")
+# install.packages("rio")
+
+# library("rio")
+# library("questionr")
+# library("datasets")
+# library("gmodels")
+# library("tidyverse")
+# library("knitr")
